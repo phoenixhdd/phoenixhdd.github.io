bf60240118214eb27313c36f13e75c113aab0284
Przemysław Jóźwik <przemyslaw.jozwik96@gmail.com>
2018-06-02 19:00:34 +0200

    Add playlist view

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,34 @@
 const playSongButton = document.querySelector('.play-song__icon');
+const menuContent = document.querySelector('.daft-player__menu');
+const playlistContent = document.querySelector('.daft-player__playlist');
+const playerHeading = document.querySelector('.daft-player__heading');
+const playlistPlayer = document.querySelector('.playlist__play-icon');
 
 const activePlayMusic = () => {
-  if(!playSongButton.classList.contains('paused')) {
+  console.log('work');
+  if(!playSongButton.classList.contains('paused') && !playlistPlayer.classList.contains('paused')) {
     playSongButton.classList.add('paused');
+    playlistPlayer.classList.add('paused');
   } else {
     playSongButton.classList.remove('paused');
+    playlistPlayer.classList.remove('paused');
+  }
+}
+
+const openMenu = () => {
+  if(!menuContent.classList.contains('open')) {
+    menuContent.classList.add('open');
+  } else {
+    menuContent.classList.remove('open');
+  }
+}
+
+const openPlaylist = () => {
+  if(!playlistContent.classList.contains('open')) {
+    playlistContent.classList.add('open');
+    playerHeading.classList.add('hide');
+  } else {
+    playlistContent.classList.remove('open');
+    playerHeading.classList.remove('hide');
   }
 }
--- js/waveform.js
+++ js/waveform.js
@@ -3,7 +3,7 @@ const waveFormContainer = document.querySelector('.daft-player__waveform');
 const applicationWidth = document.body.clientWidth / 4;
 
 const generateWaveform = () => {
-  const randomHeight = Math.floor(Math.random() * 40) + 15;
+  const randomHeight = Math.floor(Math.random() * 20) + 15;
   const newBar = document.createElement('span');
   newBar.className = 'daft-player__waveform--bar';
   newBar.style.height = randomHeight + 'px';
@@ -14,7 +14,7 @@ for (let i = 0; i <= applicationWidth; i++) {
   (function() {
     setTimeout(function() { }, i * 1000);
   })(i);
-  const randomHeight = Math.floor(Math.random() * 40) + 15;
+  const randomHeight = Math.floor(Math.random() * 20) + 15;
   waveformBar.style.height = randomHeight + 'px';
   generateWaveform();
 }
